c570562b54882c534dd7dd1c91b3cb24e28dcba2
JuanMartinUribe <jmuribef@eafit.edu.co>
2023-08-21 23:03:28 -0500

    update tip component friend description

--- src/Tip.js
+++ src/Tip.js
@@ -1,7 +1,7 @@
 export default function Tip({ tip, onTipChange, i }) {
   return (
     <div>
-      How satisfied were you?
+      How satisfied {i === 0 ? "were you" : "was your friend"}?
       <select value={tip} onChange={(e) => onTipChange(i, +e.target.value)}>
         <option value="0"> 0 </option>
         <option value="10"> 10 </option>
